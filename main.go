625213b11780423fc25457a20fefc074b3eb5130
Igor Aleksandrov <ivaleksandrov@avito.ru>
2022-07-03 22:39:39 +0300

    list and add errors

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"errors"
 	"flag"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 const filePerm = 0644
@@ -35,8 +37,9 @@ func init() {
 
 func Perform(args Arguments, writer io.Writer) error {
 	fileName, ok := args["fileName"]
-	if !ok {
-		return errors.New("invalid args")
+	fileName = strings.TrimSpace(fileName)
+	if !ok || fileName == "" {
+		return errors.New("-fileName flag has to be specified")
 	}
 
 	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, filePerm)
@@ -46,8 +49,9 @@ func Perform(args Arguments, writer io.Writer) error {
 	defer file.Close()
 
 	operation, ok := args["operation"]
-	if !ok {
-		return errors.New("invalid args")
+	operation = strings.TrimSpace(operation)
+	if !ok || operation == "" {
+		return errors.New("-operation flag has to be specified")
 	}
 
 	switch operation {
@@ -60,18 +64,36 @@ func Perform(args Arguments, writer io.Writer) error {
 		}
 	case "add":
 		item, ok := args["item"]
-		if !ok {
-			return errors.New("invalid args")
+		item = strings.TrimSpace(item)
+		if !ok || item == "" {
+			return errors.New("-item flag has to be specified")
 		}
 
-		err := add(file, item)
+		msg, err := add(file, item)
 		if err != nil {
 			return err
 		}
+		_, err = writer.Write([]byte(msg))
+		if err != nil {
+			return errors.New("writer error")
+		}
 
 		return nil
+	case "findById":
+		id, ok := args["id"]
+		id = strings.TrimSpace(id)
+		if !ok || id == "" {
+			return errors.New("-id flag has to be specified")
+		}
+
+		itemBytes, err := findById(file, id)
+
+		_, err = writer.Write(itemBytes)
+		if err != nil {
+			return errors.New("writer error")
+		}
 	default:
-		return errors.New("unknown operation")
+		return errors.New(fmt.Sprintf("Operation %s not allowed!", operation))
 	}
 
 	return nil
@@ -99,32 +121,66 @@ func list(file *os.File) ([]byte, error) {
 	return bytes, nil
 }
 
-func add(file *os.File, item string) error {
+func add(file *os.File, item string) (string, error) {
 	bytes, err := list(file)
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	var users []UserStruct
-	if err := json.Unmarshal(bytes, &users); err != nil {
-		return err
-	}
+	users := make([]UserStruct, 0)
+	json.Unmarshal(bytes, &users)
 
 	var user UserStruct
 	if err := json.Unmarshal([]byte(item), &user); err != nil {
-		return err
+		return "", err
 	}
+
+	for _, us := range users {
+		if user.Id == us.Id {
+			return fmt.Sprintf("Item with id %s already exists", user.Id), nil
+		}
+	}
+
 	users = append(users, user)
+
 	usersBytes, err := json.Marshal(users)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	if _, err := file.WriteAt(usersBytes, 0); err != nil {
-		return err
+		return "", err
 	}
 
-	return nil
+	return "", nil
+}
+
+func findById(file *os.File, id string) ([]byte, error) {
+	bytes, err := list(file)
+	if err != nil {
+		return nil, err
+	}
+
+	users := make([]UserStruct, 0)
+	json.Unmarshal(bytes, &users)
+
+	foundId := -1
+	for i, us := range users {
+		if id == us.Id {
+			foundId = i
+		}
+	}
+
+	if foundId == -1 {
+		return []byte{}, nil
+	}
+
+	usersBytes, err := json.Marshal(users[foundId])
+	if err != nil {
+		return nil, err
+	}
+
+	return usersBytes, nil
 }
 
 func main() {
